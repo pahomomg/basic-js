18adf11ebb8b62c9ba9e0113f0643a3e36837f13
pahomomg <106546045+pahomomg@users.noreply.github.com>
2022-10-11 17:54:05 +0300

    Add files via upload

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(arr) {
+  let cats = [];
+  arr.forEach((sub) => {
+    sub.forEach((e) => {
+      return e === "^^" ? cats.push(e) : "";
+    });
+  });
+  return cats.length;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
